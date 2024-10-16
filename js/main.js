192880b0f83803d74226c9b4537fc2c7fda9bd77
TarasDutchak <Tarasdutchak90@gmail.com>
2024-10-16 12:32:46 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -122,6 +122,21 @@ $(document).ready(function () {
     })
 
 
+    // скрол до елемента
+    $('.scrolldown').on('click', function() {
+
+        let href = $(this).attr('href');
+    
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        }, {
+            duration: 370,   // по умолчанию «400» 
+            easing: "linear" // по умолчанию «swing» 
+        });
+    
+        return false;
+    });
+
 
 
 });
